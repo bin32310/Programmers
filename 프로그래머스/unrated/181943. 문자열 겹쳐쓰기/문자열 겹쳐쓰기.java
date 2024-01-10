bfd16f12f470ff8e6c64dca2419f319476dcc0ac
BIN <144088728+bin32310@users.noreply.github.com>
2024-01-10 11:33:31 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 14.11 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,9 +1,6 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         String answer = "";
-//        answer = my_string.replace(my_string.substring(s,overwrite_string.length()+s),overwrite_string);
-//        System.out.println(answer);
-        answer = my_string.substring(0,s) + overwrite_string + my_string.substring(overwrite_string.length()+s);
-        return answer;
+        return my_string.substring(0,s) + overwrite_string + my_string.substring(overwrite_string.length()+s);
     }
 }
