50e4124cd24904aee27027843a1ffe5187e372eb
BIN <144088728+bin32310@users.noreply.github.com>
2024-01-15 10:17:48 +0900

    [level Lv.0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
+++ 프로그래머스/Lv.0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -1,19 +1,10 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        if(n % 2 == 0){ // 주어진 수가 짝수면 제곱+
-            while(n > 0){
-                answer += (n % 2 == 0)? n*n : 0 ;
-                n--;
-            }
+        while(n > 0){
+            answer += (n % 2 == 0)? n*n : n ;
+            n-=2;
         }
-        else{ // 주어진 수가 홀수면 +
-            while(n > 0){
-                answer += (n % 2 == 1)? n : 0 ;
-                n--;
-            }            
-        }
-        System.out.println(answer);
         return answer;
     }
 }
