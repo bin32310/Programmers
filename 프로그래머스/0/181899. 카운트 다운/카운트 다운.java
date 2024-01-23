68a2f273ef75234026aa7e5dd294e76cfb5b220a
BIN <144088728+bin32310@users.noreply.github.com>
2024-01-23 16:05:04 +0900

    [level 0] Title: 카운트 다운, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int start, int end_num) {
+        int[] answer = new int[start-end_num+1];
+        
+        for(int i=0; start >= end_num; i++){
+            answer[i] =  start;
+            start--;
+        }
+        
+        return answer;
+    }
+}
